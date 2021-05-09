535d4a3dc263871ab0acd6c2be963eb7198b402b
Muskaan-10 <muskaanhira2007@gmail.com>
2021-05-09 11:46:45 +0530

    Catapult added

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1=loadImage ("sprites/sling1.png");
+        this.sling2=loadImage ("sprites/sling2.png");
+        this.sling3=loadImage ("sprites/sling3.png");
     }
 
     fly(){
@@ -16,12 +19,32 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 200, 20);
+        image(this.sling2, 170, 20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            //strokeWeight(4);
+            stroke("#A67035");
+            //line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            if(pointA.x<200){
+                strokeWeight(7);
+                line (pointA.x-40, pointA.y+10, pointB.x-20, pointB.y+10);
+                line (pointA.x-40, pointA.y+10, pointB.x+35, pointB.y+5);
+                image(this.sling3, pointA.x-40, pointA.y+4,10,20);
+            }
+
+            else{
+                strokeWeight(2);
+                line (pointA.x+40, pointA.y+10, pointB.x-20, pointB.y+10);
+                line (pointA.x+40, pointA.y+10, pointB.x+35, pointB.y+5);
+                image(this.sling3, pointA.x+40, pointA.y+4,10,20);
+            }
+
         }
+
+        
     }
     
 }
